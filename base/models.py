f6f3096fe052bf1b6ab464bcf4c37a2e09bf6d74
rxzhik <rashikraj12@gmail.com>
2024-06-29 19:40:21 +1000

    Created the review model

--- base/models.py
+++ base/models.py
@@ -13,23 +13,19 @@ class Course(models.Model):
     numReviews = models.IntegerField(null=True,blank=True,default=0)
     price = models.DecimalField(max_digits=12,decimal_places=2,null=True,blank=True)
     createdAt = models.DateTimeField(auto_now_add=True)
-    _id = models.AutoField(primary_key=True,editable=False)
-
     def __str__(self):
         return self.name +" | "  + str(self.price)
 
-
+    
 class Review(models.Model):
-    product = models.ForeignKey(Product,on_delete=models.SET_NULL,null=True)
-    user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
-    name = models.CharField(max_length=200,null=True,blank=True)
-    rating =  models.IntegerField(null=True,blank=True,default=0)
-    comment = models.TextField(null=True,blank=True)
-    createdAt = models.DateTimeField(auto_now_add=True)
-    _id =  models.AutoField(primary_key=True,editable=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
+    rating = models.IntegerField(null=True, blank=True, default=0)
+    comment = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return str(self.rating)
+        return f"{self.course.title} - {self.rating}"
 
 
 class Order(models.Model):
