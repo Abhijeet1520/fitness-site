2239f50f706913b2b323461eb779b2334a09f03a
rxzhik <rashikraj12@gmail.com>
2024-07-02 18:44:45 +1000

    Creaetd basic serializers for the models

--- backend/settings.py
+++ backend/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+
+#loading environment variables
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i-b2o_4@ru#jr_y)vbhdjng$607jjufk4i8b+*wrk0p&!ae%-e'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- base/serializers.py
+++ base/serializers.py
@@ -37,58 +37,118 @@ class UserSerializerWithToken(UserSerializer):
         return str(token.access_token)
 
 
+# class ReviewSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Review
+#         fields = '__all__'
+
+# class ProductSerializer(serializers.ModelSerializer):
+#     reviews = serializers.SerializerMethodField(read_only= True)
+#     class Meta:
+#         model = Product 
+#         fields = '__all__'
+
+#     def get_reviews(self,obj):
+#         reviews = obj.review_set.all()
+#         serializer = ReviewSerializer(reviews,many=True)
+#         return serializer.data
+
+# class ShippingAddressSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = ShippingAddress
+#         fields = '__all__'
+
+# class OrderItemSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = OrderItem
+#         fields = '__all__'
+
+# class OrderSerializer(serializers.ModelSerializer):
+#     orderItems = serializers.SerializerMethodField(read_only=True)
+#     shippingAddress = serializers.SerializerMethodField(read_only=True)
+#     User = serializers.SerializerMethodField(read_only=True)
+
+#     class Meta:
+#         model = Order
+#         fields = '__all__'
+
+#     def get_orderItems(self,obj):
+#         items = obj.orderitem_set.all()
+#         serializer = OrderItemSerializer(items,many=True)
+#         return serializer.data
+
+#     def get_shippingAddress(self,obj):
+#         try:
+#             address = ShippingAddressSerializer(obj.shippingaddress,many=False).data
+#         except:
+#             address = False
+#         return address
+
+#     def get_User(self,obj):
+#         items = obj.user
+#         serializer = UserSerializer(items,many=False)
+#         return serializer.data
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Course
+        fields=['id', 'user', 'name', 'image', 'description', 'rating', 'numReviews', 'price', 'created_at']
+
+
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Review
-        fields = '__all__'
+        model=Review
+        fields=['id', 'user', 'course', 'rating', 'comment', 'created_at']
 
-class ProductSerializer(serializers.ModelSerializer):
-    reviews = serializers.SerializerMethodField(read_only= True)
+class OrderSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Product 
-        fields = '__all__'
-
-    def get_reviews(self,obj):
-        reviews = obj.review_set.all()
-        serializer = ReviewSerializer(reviews,many=True)
-        return serializer.data
-
-class ShippingAddressSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ShippingAddress
-        fields = '__all__'
+        model=Order
+        fields=['id', 'user', 'shipping_address', 'created_at', 'payment']
 
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
-        model = OrderItem
-        fields = '__all__'
+        model=OrderItem
+        fields=['id', 'order', 'course', 'price', 'image', 'created_at']
 
-class OrderSerializer(serializers.ModelSerializer):
-    orderItems = serializers.SerializerMethodField(read_only=True)
-    shippingAddress = serializers.SerializerMethodField(read_only=True)
-    User = serializers.SerializerMethodField(read_only=True)
 
+class CartSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Order
-        fields = '__all__'
+        model=Cart
+        fields=['id', 'user', 'created_at']
 
-    def get_orderItems(self,obj):
-        items = obj.orderitem_set.all()
-        serializer = OrderItemSerializer(items,many=True)
-        return serializer.data
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=CartItem
+        fields=['id', 'cart', 'course', 'created_at']
 
-    def get_shippingAddress(self,obj):
-        try:
-            address = ShippingAddressSerializer(obj.shippingaddress,many=False).data
-        except:
-            address = False
-        return address
 
-    def get_User(self,obj):
-        items = obj.user
-        serializer = UserSerializer(items,many=False)
-        return serializer.data
+class ShippingAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=ShippingAddress
+        fields=['id', 'user', 'address', 'city', 'state', 'postalCode', 'country', 'created_at']
 
 
 
+class WeekSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Week
+        fields=['id', 'course', 'week_number', 'created_at']
 
+
+class DaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Day
+        fields=['id', 'description', 'week']
+
+
+class ExerciseSerializer(serializers.Serializer):
+    class Meta:
+        model=Exercise
+        fields=['id', 'week', 'day', 'title', 'type', 'sets', 'reps', 'duration', 'description', 'video_url', 'created_at']
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Payment
+        fields=['id', 'user', 'order', 'payment_method', 'tax_price', 'shipping_price', 'total_price', 'is_paid', 'paid_at', 'created_at']
