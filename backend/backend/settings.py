7d180fa3fd232edc772193783d2b08b4d4a4b5d5
rxzhik <rashikraj12@gmail.com>
2024-07-12 12:11:57 +1000

    added 2admin users and changed token lifetime

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,8 +74,8 @@ from datetime import timedelta
 ...
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=3),
 }
 
 MIDDLEWARE = [
