ef6bf8d57bd842efa70d3b00377f6fa9547b46b0
udit710 <s3933905@student.rmit.edu.au>
2024-07-12 12:04:23 +1000

    Fixed button issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: '0px 0px 0px 0.5px rgba(50, 50, 93, 0.1), 0px 2px 5px 0px rgba(50, 50, 93, 0.1), 0px 1px 1.5px 0px rgba(0, 0, 0, 0.07)',
+      },
+      borderRadius: {
+        '7': '7px',
+      },
+    },
   },
   plugins: [
     require('daisyui'),
