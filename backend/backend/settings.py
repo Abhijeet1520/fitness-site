78ee5b51bd1c068c7d77e764fcd6d1dd7168552a
rxzhik <rashikraj12@gmail.com>
2024-07-17 00:05:06 +1000

    Connectd RDS AWS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -120,8 +120,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'HOST': os.getenv('DB_HOST'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
