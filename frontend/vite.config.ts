f7d22c9bee912625a6d570cff69ec12b1ef364f3
Abhijeet1520 <abhijeetkumar12346@gmail.com>
2024-07-27 01:38:03 +1000

    Add import aliases for better module resolution

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    // Adding Import Aliases
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@interfaces": path.resolve(__dirname, "./src/interfaces"),
+            "@pages": path.resolve(__dirname, "./src/pages"),
+            "@json": path.resolve(__dirname, "./src/json"),
+            "@services": path.resolve(__dirname, "./src/services"),
+        },
+    },
+
+    plugins: [react()],
+});
