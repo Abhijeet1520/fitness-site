37e3530ecc7025cc385a95b7f72ba411f5a5cabd
Abhijeet1520 <abhijeetkumar12346@gmail.com>
2024-07-28 17:37:50 +1000

    Update ExerciseCard component to toggle description visibility

--- frontend/src/components/exercises/exerciseCard.tsx
+++ frontend/src/components/exercises/exerciseCard.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import ReactPlayer from 'react-player';
 import { Exercise } from "../../interfaces/exercise";
 
 const ExerciseCard = ({ exercise }: { exercise: Exercise }) => {
+  const [showDescription, setShowDescription] = useState(false);
+
+  const toggleDescription = () => {
+    setShowDescription(!showDescription);
+  };
+
   return (
-    <div className={exercise.type === "cooldown" ? "card bg-blue-300 p-10 my-10" : exercise.type === "warmup" ? "card bg-orange-300 p-10 my-10" : "card bg-zinc-300 p-10 my-10"}>
-      <h2 className="text-xl font-bold text-left">{exercise.name}</h2>
-      <p><strong>Duration:</strong> {exercise.duration} minutes</p>
+    <div className="card p-10 my-10 relative">
+      <div className={`absolute left-0 top-0 h-full w-2 ${exercise.type === "cooldown" ? "bg-blue-300" : exercise.type === "warmup" ? "bg-orange-300" : "bg-zinc-300"}`}></div>
+      <h2 className="text-xl font-bold text-left ml-4">{exercise.title}</h2>
+      <p className="ml-4 text-sm">Duration: {exercise.duration} minutes</p>
       {exercise.complete_url && (
         <ReactPlayer
           url={exercise.complete_url}
@@ -22,7 +30,15 @@ const ExerciseCard = ({ exercise }: { exercise: Exercise }) => {
           }}
         />
       )}
-      <p><strong>Description: </strong>{exercise.description}</p>
+      <div className="text-center mt-4">
+        <button
+          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-300"
+          onClick={toggleDescription}
+        >
+          Mira's Instructions
+        </button>
+      </div>
+      {showDescription && <p className="ml-4 mt-4">{exercise.description}</p>}
     </div>
   );
 };
