d7934f4a5a49daaefc44ec8f396937d06b3666ca
Abhijeet1520 <abhijeetkumar12346@gmail.com>
2024-07-30 00:50:56 +1000

    Add default Nginx configuration for altemirafitness.fit

---
+++ backend/default_nginx.sh
@@ -0,0 +1,31 @@
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    server_name altemirafitness.fit www.altemirafitness.fit;
+
+    location / {
+        return 301 https://$host$request_uri;
+    }
+}
+
+server {
+    listen 443 ssl default_server;
+    listen [::]:443 ssl default_server;
+    server_name altemirafitness.fit www.altemirafitness.fit;
+
+    ssl_certificate /etc/letsencrypt/live/altemirafitness.fit/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/altemirafitness.fit/privkey.pem;
+    ssl_dhparam /etc/nginx/ssl/dhparam.pem;
+
+    root /home/ubuntu/fitness-site/backend;
+
+    location / {
+        try_files $uri $uri/ /index.html;
+        include proxy_params;
+        proxy_pass http://unix:/home/ubuntu/fitness-site/backend/fitness-site.sock;
+    }
+
+    location /static/ {
+        alias /home/ubuntu/fitness-site/backend/static/;
+    }
+}
