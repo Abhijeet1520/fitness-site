76bc483756daad1ff38bf2d55fa504c152b86d8c
Vidyut Venkatesan <vidyut0903@gmail.com>
2024-07-31 20:41:32 +1000

    Added 404 page

---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+  return (
+    <div className="mt-20">
+        <h1 className="text-[#008026]">404</h1>
+        <br/>
+        <h2>The page you are looking for can't be found on the server</h2>
+        <a href="/" className="text-blue-500 hover:underline">Go back home</a>
+    </div>
+  )
+}
+
+export default NotFound
